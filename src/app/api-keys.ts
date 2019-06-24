4ddbaf61a9298cad94ee112a36fa75d2b8fe7636
Student <example@example.com>
2019-06-24 11:34:49 -0700

    add json file with women clinics

---
+++ src/app/api-keys.ts
@@ -0,0 +1,9 @@
+export const masterFirebaseConfig = {
+  apiKey: "AIzaSyCZL36dj9jDpFgero2cRQ32PlM18Lnwa48",
+  authDomain: "planbuddy-4c5ac.firebaseapp.com",
+  databaseURL: "https://planbuddy-4c5ac.firebaseio.com",
+  projectId: "planbuddy-4c5ac",
+  storageBucket: "planbuddy-4c5ac.appspot.com",
+  messagingSenderId: "304901545171",
+  appId: "1:304901545171:web:1f7da2b4c09f209d"
+};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ClinicListComponent } from './clinic-list/clinic-list.component';
-import { ClinicDetailComponent } from './clinic-detail/clinic-detail.component';
-import { ReferencesListComponent } from './references-list/references-list.component';
-import { ReferenceDetailComponent } from './reference-detail/reference-detail.component';
-import { AboutComponent } from './about/about.component';
-import { AdminComponent } from './admin/admin.component';
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClinicListComponent,
-    ClinicDetailComponent,
-    ReferencesListComponent,
-    ReferenceDetailComponent,
-    AboutComponent,
-    AdminComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
