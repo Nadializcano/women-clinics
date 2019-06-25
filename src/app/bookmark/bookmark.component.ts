45bf6368921a102ad0e1b5bca8e8d538d598bede
Student <example@example.com>
2019-06-25 13:54:56 -0700

    add delete for bookmarked clinics

--- src/app/bookmark/bookmark.component.ts
+++ src/app/bookmark/bookmark.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BookmarkService } from '../bookmark.service';
 import { FirebaseListObservable } from 'angularfire2/database';
+import { Clinic } from '../models/clinic.model';
 
 @Component({
   selector: 'app-bookmark',
@@ -15,6 +16,12 @@ export class BookmarkComponent implements OnInit {
 
   ngOnInit() {
     this.clinics = this.bookmarkService.getClinics();
+  }
+
+  beginDeletingClinic(clinicToDelete: Clinic){
+    if(confirm("Are you sure you want to delete this bookmark?")){
+      this.bookmarkService.deleteBookmark(clinicToDelete);
+    }
   }
 
 }
