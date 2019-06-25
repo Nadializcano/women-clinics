4e0ebe6601e5e6e3c3ae82d39271f15f7f5b649d
Student <example@example.com>
2019-06-25 10:40:44 -0700

    login works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,36 @@
  @Component({
    selector: 'app-root',
    templateUrl: './app.component.html',
-   styleUrls: ['./app.component.css']
+   styleUrls: ['./app.component.css'],
+    providers: [AuthenticationService]
+
  })
  export class AppComponent {
    title = 'app';
+   user;
+private isLoggedIn: Boolean;
+private userName: String;
+
+constructor(public authService: AuthenticationService, private router: Router) {
+  this.authService.user.subscribe(user => {
+    if (user == null) {
+      this.isLoggedIn = false;
+      // this.router.navigate(['public']);
+    } else {
+      this.isLoggedIn = true;
+      this.userName = user.displayName;
+      // this.router.navigate([]);
+    }
+  });
+}
+
+
+   login() {
+  this.authService.login();
+}
+
+logout() {
+  this.authService.logout();
+
+}
  }
